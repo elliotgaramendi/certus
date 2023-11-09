901cf9ce2679b9fe49abd35fb3d0096d3a4692db
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-09 08:14:58 -0500

    refactor: Create data util

---
+++ utils/dataUtils.js
@@ -0,0 +1,26 @@
+export const socialMedia = [
+  {
+    id: 1,
+    href: 'https://www.youtube.com/@elliotgaramendi',
+    title: 'Red social de YouTube',
+    content: '<i class="bi bi-youtube"></i>'
+  },
+  {
+    id: 2,
+    href: 'https://github.com/elliotgaramendi',
+    title: 'Red social de GitHub',
+    content: '<i class="bi bi-github"></i>'
+  },
+  {
+    id: 3,
+    href: 'https://www.linkedin.com/in/elliotgaramendi/',
+    title: 'Red social de LinkedIn',
+    content: '<i class="bi bi-linkedin"></i>'
+  },
+  {
+    id: 4,
+    href: 'https://www.instagram.com/elliotgaramendi/',
+    title: 'Red social de Instagram',
+    content: '<i class="bi bi-instagram"></i>'
+  }
+];
