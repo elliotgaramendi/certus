9d281a445a5c0d9eb13555ed948f32dee00c2421
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-09 08:41:56 -0500

    feat: Create team section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-import { socialMedia } from '../utils/dataUtils.js';
+import { socialMedia, teams } from '../utils/dataUtils.js';
 
 const socialMediaList = document.getElementById('socialMediaList');
+const teamList = document.getElementById('teamList');
 
 socialMedia.forEach(element => {
   const { id, href, title, content } = element;
   socialMediaList.innerHTML += `
-    <li key=${id}>
+    <li key="${id}">
       <a
         href="=${href}"
         target="_blank"
@@ -15,6 +16,28 @@ socialMedia.forEach(element => {
       >
         ${content}
       </a>
+    </li>
+  `;
+});
+
+teams.forEach(element => {
+  const { id, title, slug, subtitle, description, image } = element;
+  teamList.innerHTML += `
+    <li
+      class="card"
+      key="${id}"
+    >
+      <img
+        src="${image}"
+        alt="${title}"
+        class="img img--card"
+      />
+      <div class="card__body">
+        <h3 class="card__title">${title}</h3>
+        <h4 class="card__subtitle">${subtitle}</h4>
+        <p class="card__text">${description}</p>
+        <a href="./teams/${slug}" class="button">Proyectos</a>
+      </div>
     </li>
   `;
 });
