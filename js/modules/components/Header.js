ff22c034c3118cc34eac07ed08295d7e2343729a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-30 09:09:26 -0500

    feat: Create JavaScript for P28 page

--- js/modules/components/Header.js
+++ js/modules/components/Header.js
@@ -1,9 +1,10 @@
 const Header = () => {
-  const headerElement = document.querySelector('.header');
+  const header = document.querySelector('.header');
 
   window.addEventListener('scroll', () => {
-    headerElement.classList.toggle('header--scroll', window.scrollY > 0);
+    header.classList.toggle('header--scroll', window.scrollY > 0);
   });
+
 };
 
 export default Header;
---
+++ js/pages/P28.js
@@ -0,0 +1,5 @@
+import Header from '../modules/components/Header.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  Header();
+});
