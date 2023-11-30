dbae3b4ba1d792c87f62bd29d235cf565194e09c
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-30 09:54:05 -0500

    feat: Update Header

--- js/modules/components/Header.js
+++ js/modules/components/Header.js
@@ -1,8 +1,10 @@
 const Header = () => {
   const header = document.querySelector('.header');
+  const nav = document.querySelector('.nav');
 
   window.addEventListener('scroll', () => {
     header.classList.toggle('header--scroll', window.scrollY > 0);
+    nav.classList.toggle('nav--scroll', window.scrollY > 0);
   });
 
 };
